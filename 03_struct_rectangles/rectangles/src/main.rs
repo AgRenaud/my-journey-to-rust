125172695aed54acd4c752a93b7a615c520b60fd
AgRenaud <renaud.aguirrechambilla@gmail.com>
2021-11-27 12:24:07 +0100

    add rectangles struct

---
+++ 03_struct_rectangles/rectangles/src/main.rs
@@ -0,0 +1,57 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    let scale = 2;
+    let rect2 = Rectangle {
+        width: dbg!(5 * scale),
+        height: 40,
+    };
+
+    dbg!(&rect2);
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("rect1 is {:?}", rect1);
+    println!("rect2 is {:?}", rect2);
+    println!("rect3 is {:?}", rect3);
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let square = Rectangle::square(10);
+    println!("square is {:?}", square);
+}
