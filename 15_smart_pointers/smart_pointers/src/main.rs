83c666484679f768b81a006974acb96b97c58284
AgRenaud <renaud.aguirrechambilla@gmail.com>
2022-06-25 10:44:51 +0200

    reference counting Rc example

--- 15_smart_pointers/smart_pointers/src/main.rs
+++ 15_smart_pointers/smart_pointers/src/main.rs
@@ -1,5 +1,6 @@
 use std::mem::drop;
 use std::ops::Deref;
+use std::rc::Rc;
 
 struct MyBox<T>(T);
 
@@ -30,6 +31,13 @@ impl Drop for CustomSmartPointer {
         println!("Dropping CustomSmartPointer with data `{}`!", self.data);
     }
 }
+
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
 
 fn main() {
     // Store data to the heap
@@ -68,5 +76,18 @@ fn main() {
         };
         drop(c);
         println!("CustomSmartPointers created.");
+    }
+
+    {
+        let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+        println!("count after creating a = {}", Rc::strong_count(&a));
+        let b = Cons(3, Rc::clone(&a));
+        println!("count after creating b = {}", Rc::strong_count(&a));
+        {
+            let c  = Cons(4, Rc::clone(&a));
+            println!("count after creating c = {}", Rc::strong_count(&a));
+        }
+        println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+        // let c = Cons(4, a.clone()); // Not recommanded 
     }
 }
